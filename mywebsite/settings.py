d5f29adf750fef05c4c806af878c33eab3d8d0bf
RΨST-4M <59933997+Rustam-Z@users.noreply.github.com>
2020-08-15 15:02:27 +0500

    Update settings.py

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'y=uv+*00!9dpni1sw&f(q&pv2d@2&==@lt&3)=_ze(_m2#6^9!'
-# pw: nyoocrotvgcqytaw
+# pwnyoocrotvgcqytaw
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
